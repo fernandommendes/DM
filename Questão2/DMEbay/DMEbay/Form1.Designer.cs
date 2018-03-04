140615d81fdbb2f4abc0b5ba179f22e666bd7ec5
Fernando Mendes <fernando_moreiram@hotmail.com>
2018-03-04 14:30:43 -0300

    Questao 2

---
+++ Questão2/DMEbay/DMEbay/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DMEbay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(365, 20);
+            this.textBox1.TabIndex = 0;
+            //this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 392);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Questão2/DMEbay/DMEbay/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using HtmlAgilityPack;
+using System.Net;
+
+namespace DMEbay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+             id da barra de pesquisa = gh-ac
+             */
+
+            List<string> ebay = new List<string>();
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            String barraPesquisa = "";
+            List<string> carrinho = new List<string>();
+            XmlDocument page = new XmlDocument();
+
+            string link = "https://www.ebay.com";
+            using (WebClient client = new WebClient())
+            {                
+                doc.LoadHtml(link);
+            }
+
+            HtmlAgilityPack.HtmlNodeCollection(link);
+
+            //XmlNodeList nodeList = page.SelectNodes("//input[@id='gh - ac']");
+
+            //XmlNode barraPesquisa = page.SelectSingleNode("//input[@id='gh - ac']");
+
+            MessageBox.Show(Convert.ToString(barraPesquisa));
+        }
+    }
+}
